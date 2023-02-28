cad6b1459806fa75089735c4040ced6455a61a4d
Lucas <lucaspaulo_212@hotmail.com>
2023-02-28 08:19:34 -0300

    git and bodyParser middleware added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require("express");
-
+const express = require('express');
 const compression = require('compression');
-
-const projectsRouter = require('./routes/projectsRouter')
+const projectsRouter = require('./routes/projectsRouter');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(compression());
 app.use(projectsRouter);
+app.use(express.json());
+app.use(bodyParser.json());
 
 module.exports = app;
